c5453077f10ea175d96d6947edb5da39ecbea2a2
Pedro Dias <pedroddias98@gmail.com>
2024-10-29 21:29:45 -0300

    Adicionando validações e testes unitários para Portion

--- app/models/portion.rb
+++ app/models/portion.rb
@@ -1,4 +1,21 @@
 class Portion < ApplicationRecord
   belongs_to :item
   validates :description, :price, presence: true
+  before_validation :validate_portion_description
+  before_validation :validate_price
+
+  private
+
+  def validate_portion_description
+    portion = Portion.find_by(item_id: self.item.id, description: self.description)
+    if portion && portion.id != self.id
+      self.errors.add :description, 'já está sendo usada por esse item.'
+    end
+  end
+
+  def validate_price
+    if self.price && self.price <= 0
+      self.errors.add :price, 'é inválido.'
+    end
+  end
 end
--- spec/models/portion_spec.rb
+++ spec/models/portion_spec.rb
@@ -1,4 +1,74 @@
 require 'rails_helper'
 
 RSpec.describe Portion, type: :model do
+  describe '#valid' do
+    context 'presence' do
+      it 'falso quando descrição ficar em branco' do
+        user = User.create!(name: 'Pedro', surname: 'Dias', social_number: '133.976.443-13',
+                        email: 'pedro@email.com', password: 'passwordpass')
+        restaurant = Restaurant.create!(legal_name: 'Rede RonaldMc Alimentos', restaurant_name: 'RonaldMc',
+                                        registration_number: '41.684.415/0001-09', email: 'contato@RonaldMc.com',
+                                        phone_number: '2128270790', address: 'Av Mario, 30', user: user)
+        user.update(registered_restaurant: true)
+        dish = Dish.create!(restaurant_id: restaurant.id, name: 'Croquete', 
+                            description: 'Croquete de carne', calories: 274)
+        portion = Portion.new(item: dish, description: '', price: 10.00)
+
+        
+        expect(portion).not_to be_valid
+        expect(portion.errors.include? :description).to eq true
+      end
+
+      it 'falso quando preço ficar em branco' do
+        user = User.create!(name: 'Pedro', surname: 'Dias', social_number: '133.976.443-13',
+                        email: 'pedro@email.com', password: 'passwordpass')
+        restaurant = Restaurant.create!(legal_name: 'Rede RonaldMc Alimentos', restaurant_name: 'RonaldMc',
+                                        registration_number: '41.684.415/0001-09', email: 'contato@RonaldMc.com',
+                                        phone_number: '2128270790', address: 'Av Mario, 30', user: user)
+        user.update(registered_restaurant: true)
+        beverage = Beverage.create!(restaurant_id: restaurant.id, name: 'Fanta', 
+                            description: 'Fanta laranja lata', calories: 300)
+        portion = Portion.new(item: beverage, description: '3 unid.', price: '')
+
+        
+        expect(portion).not_to be_valid
+        expect(portion.errors.include? :price).to eq true
+      end
+    end
+
+    context 'uniqueness' do
+      it 'falso quando descrição está sendo usada por outra porção do item' do
+        user = User.create!(name: 'Pedro', surname: 'Dias', social_number: '133.976.443-13',
+                        email: 'pedro@email.com', password: 'passwordpass')
+        restaurant = Restaurant.create!(legal_name: 'Rede RonaldMc Alimentos', restaurant_name: 'RonaldMc',
+                                        registration_number: '41.684.415/0001-09', email: 'contato@RonaldMc.com',
+                                        phone_number: '2128270790', address: 'Av Mario, 30', user: user)
+        user.update(registered_restaurant: true)
+        dish = Dish.create!(restaurant_id: restaurant.id, name: 'Croquete', 
+                            description: 'Croquete de carne', calories: 274)
+        Portion.create!(item: dish, description: '3 unid', price: 10.00)
+        other_portion = Portion.new(item: dish, description: '3 unid', price: 4.00)
+
+        
+        expect(other_portion).not_to be_valid
+        expect(other_portion.errors.include? :description).to eq true
+      end
+    end
+
+    it 'falso quando preço for negativo' do
+      user = User.create!(name: 'Pedro', surname: 'Dias', social_number: '133.976.443-13',
+                      email: 'pedro@email.com', password: 'passwordpass')
+      restaurant = Restaurant.create!(legal_name: 'Rede RonaldMc Alimentos', restaurant_name: 'RonaldMc',
+                                      registration_number: '41.684.415/0001-09', email: 'contato@RonaldMc.com',
+                                      phone_number: '2128270790', address: 'Av Mario, 30', user: user)
+      user.update(registered_restaurant: true)
+      beverage = Beverage.create!(restaurant_id: restaurant.id, name: 'Fanta', 
+                          description: 'Fanta laranja lata', calories: 300)
+      portion = Portion.new(item: beverage, description: '3 unid.', price: -10.00)
+
+      
+      expect(portion).not_to be_valid
+      expect(portion.errors.include? :price).to eq true
+    end
+  end
 end
